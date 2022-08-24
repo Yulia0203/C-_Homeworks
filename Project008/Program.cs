0b8fc65a5d005912c1d48f2ddfe3f10cdf5ae2ad
Yulia <mamaeva.jula@gmail.com>
2022-08-24 16:14:52 +0300

    дз1_урок 3_

--- Project008/Program.cs
+++ Project008/Program.cs
@@ -3,23 +3,17 @@
 //12821 -> да
 //23432 -> да
 
-internal class Program
+Console.WriteLine("Введите пятизначное число:");
+string str = Console.ReadLine();
+int a = 0;
+for (int i = 0; i < str.Length / 2; i++)
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Введите пятизначное число:");
-        string str = Console.ReadLine();
-        int a = 0;
-        for (int i = 0; i < str.Length / 2; i++)
+    if (str.Substring(i, 1) != (str.Substring(str.Length - 1 - i, 1)))
         {
-            if (str.Substring(i, 1) != str.Substring(str.Length - 1 - i, 1))
-            {
-                Console.WriteLine("Число {0} не является палиндромом", str);
-                break;
-            }
-            else
-                a = 1;
+            Console.WriteLine("Число {0} не является палиндромом", str);
+            break;
         }
-        if (a == 1) Console.WriteLine("Число {0} является палиндромом", str);
-    }
+    else
+    a = 1;
 }
+if (a == 1) Console.WriteLine("Число {0} является палиндромом", str);
